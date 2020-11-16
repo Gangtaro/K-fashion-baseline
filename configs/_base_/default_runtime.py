0e554e6cde2acdfec540bb5ba09489f9907e2d24
Gyeong-tae Im <45648852+Gangtaro@users.noreply.github.com>
2020-11-16 18:26:59 +0900

    Update default_runtime.py

--- configs/_base_/default_runtime.py
+++ configs/_base_/default_runtime.py
@@ -138,7 +138,7 @@ total_epochs = 1 #12
 
 
 dataset_type = 'CocoDataset'
-data_root = '/content/drive/My Drive/k-fashion/'
+data_root = '/content/drive/My Drive/datasets/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
